735c2f315c86c5e881110cab583531ce507fdfdd
Lukas Winkler <git@lw1.at>
2025-06-08 16:12:17 +0200

    workaround for row counting in inspect
    see https://github.com/simonw/datasette/issues/2485

--- datasette/cli.py
+++ datasette/cli.py
@@ -148,6 +148,7 @@ async def inspect_(files, sqlite_extensions):
     app = Datasette([], immutables=files, sqlite_extensions=sqlite_extensions)
     data = {}
     for name, database in app.databases.items():
+        database.count_limit = 100000000
         counts = await database.table_counts(limit=3600 * 1000)
         data[name] = {
             "hash": database.hash,
